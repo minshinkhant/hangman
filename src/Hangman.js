33ef46c0368c68cdfb27009a191a4200b3444685
Lauren Mayers <lauren.mayers@shopify.com>
2020-06-24 11:41:27 -0400

    Replace blanks on mysteryWord

--- src/Hangman.js
+++ src/Hangman.js
@@ -4,29 +4,74 @@ import './Hangman.css';
 
 export default function Hangman() {
   const [word, setWord] = useState(null);
-  const [hiddenWord, setHiddenWord] = useState(null);
+  const [mysteryWord, setMysteryWord] = useState(null);
   const [gameInProgress, setGameInProgress] = useState(false);
 
   return (
     <>
-      {!gameInProgress && <button onClick={getRandomWord}>New Game</button>}
-      {hiddenWord}
-      <div className="Board"></div>
+      <div className="Board">
+        <div className="Word">
+          {gameInProgress ? (
+            <>
+              <p className="MysteryWord">{mysteryWord}</p>
+              <input
+                type="text"
+                className="Input"
+                onChange={handleGuess}
+                maxLength={1}
+              />
+            </>
+          ) : (
+            <button className="Button" onClick={getRandomWord}>
+              New Game
+            </button>
+          )}
+        </div>
+
+        <div className="Hangman">
+          <div className="Hangman__gallows">
+            <div className="Hangman__head">
+              <div className="Hangman__torso"></div>
+              <div className="Hangman__left-arm"></div>
+              <div className="Hangman__right-arm"></div>
+              <div className="Hangman__left-leg"></div>
+              <div className="Hangman__right-leg"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 
   function getRandomWord() {
     setGameInProgress(true);
+
     const randomWord = dictionary[
       Math.floor(Math.random() * dictionary.length)
     ].split('');
     setWord(randomWord);
-    maskWord();
+
+    setMysteryWord(new Array(randomWord.length).fill('_'));
   }
 
-  function maskWord() {
-    if (!word) return;
-    const maskedWord = word.map((letter) => (letter = '_'));
-    setHiddenWord(maskedWord);
+  function handleGuess(event) {
+    const guessedLetter = event.target.value;
+
+    console.log('word:', word);
+    console.log('mysteryWord:', mysteryWord);
+    console.log('guessedLetter', guessedLetter);
+
+    const matchIndices = word.reduce(
+      (acc, letter, index) =>
+        letter === guessedLetter ? [...acc, index] : acc,
+      []
+    );
+
+    const mysteryWordCopy = mysteryWord;
+    matchIndices.map((matchIndex) =>
+      mysteryWordCopy.splice(matchIndex, 1, guessedLetter)
+    );
+
+    setMysteryWord(mysteryWordCopy);
   }
 }
