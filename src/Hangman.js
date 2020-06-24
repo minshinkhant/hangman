4f38fc314040209e18e3130ca0f5e3a367b65d86
Lauren Mayers <lauren.a.mayers@gmail.com>
2020-06-24 14:41:41 -0400

    Handle duplicate guesses, show guessed letter bank

--- src/Hangman.js
+++ src/Hangman.js
@@ -1,29 +1,74 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {dictionary} from './words.json';
 import './Hangman.css';
 
-export default function Hangman() {
-  const [word, setWord] = useState(null);
-  const [mysteryWord, setMysteryWord] = useState(null);
-  const [gameInProgress, setGameInProgress] = useState(false);
+const HANGMAN_COMPLETE_VALUE = 6;
 
+export default function Hangman() {
+  const [word, setWord] = useState();
+  const [wordBlanks, setWordBlanks] = useState();
+  const [inputValue, setInputValue] = useState('');
+  const [hangmanProgress, setHangmanProgress] = useState(0);
+  const [gameInProgress, setGameInProgress] = useState(false);
+  const [gameWon, setGameWon] = useState();
+  const [guessedLetters, setGuessedLetters] = useState({
+    correct: [],
+    incorrect: [],
+  });
+  const [showRepeatGuessMessage, setShowRepeatGuessMessage] = useState(false);
+
+  useEffect(() => {
+    if (
+      hangmanProgress < HANGMAN_COMPLETE_VALUE &&
+      !wordBlanks?.includes('_')
+    ) {
+      setGameInProgress(false);
+      setGameWon(true);
+      setHangmanProgress(0);
+      setGuessedLetters({correct: [], incorrect: []});
+    }
+  }, [hangmanProgress, wordBlanks]);
+
+  useEffect(() => {
+    if (hangmanProgress === HANGMAN_COMPLETE_VALUE) {
+      setGameInProgress(false);
+      setGameWon(false);
+      setHangmanProgress(0);
+      setGuessedLetters({correct: [], incorrect: []});
+    }
+  }, [hangmanProgress, word]);
+
+  const guessedLetterBank = formattedGuessedLetters();
+
+  const gameOverMessage = gameWon
+    ? 'You win!'
+    : `You lose! The word was: ${word?.join('')}`;
   return (
     <div className="Board">
       <div className="Word">
+        {gameWon !== undefined && (
+          <p className="GameOverMessage">{gameOverMessage}</p>
+        )}
         {gameInProgress ? (
           <>
-            <p className="MysteryWord">{mysteryWord}</p>
+            <p className="WordBlanks">{wordBlanks}</p>
             <input
               type="text"
               className="Input"
-              onChange={handleGuess}
+              onChange={handleInputChange}
+              onKeyDown={handleGuess}
+              value={inputValue}
               maxLength={1}
             />
+            {showRepeatGuessMessage && <p>Letter already guessed</p>}
+            <div className="GuessedLetters">{guessedLetterBank}</div>
           </>
         ) : (
-          <button className="Button" onClick={getRandomWord}>
-            New Game
-          </button>
+          <>
+            <button className="Button" onClick={getRandomWord}>
+              New Game
+            </button>
+          </>
         )}
       </div>
 
@@ -41,7 +86,78 @@ export default function Hangman() {
     </div>
   );
 
+  function handleInputChange(event) {
+    const {value} = event.target;
+    setShowRepeatGuessMessage(false);
+    setInputValue(value);
+  }
+
+  function handleGuess(event) {
+    if (event.keyCode !== 13) return;
+
+    if (word.includes(inputValue)) {
+      if (guessedLetters.correct.includes(inputValue)) {
+        setShowRepeatGuessMessage(true);
+      } else {
+        setGuessedLetters({
+          ...guessedLetters,
+          correct: [...guessedLetters.correct, inputValue],
+        });
+      }
+    } else {
+      if (guessedLetters.incorrect.includes(inputValue)) {
+        setShowRepeatGuessMessage(true);
+      } else {
+        setHangmanProgress(hangmanProgress + 1);
+        setGuessedLetters({
+          ...guessedLetters,
+          incorrect: [...guessedLetters.incorrect, inputValue],
+        });
+      }
+    }
+
+    if (
+      guessedLetters.correct.includes(inputValue) ||
+      guessedLetters.incorrect.includes(inputValue)
+    ) {
+      setShowRepeatGuessMessage(true);
+    }
+
+    const matchIndices = word.reduce(
+      (acc, letter, index) => (letter === inputValue ? [...acc, index] : acc),
+      []
+    );
+
+    const wordBlanksCopy = wordBlanks;
+    matchIndices.map((matchIndex) =>
+      wordBlanksCopy.splice(matchIndex, 1, inputValue)
+    );
+
+    setWordBlanks(wordBlanksCopy);
+    setInputValue('');
+  }
+
+  function formattedGuessedLetters() {
+    const combinedGuesses = [
+      ...guessedLetters.correct,
+      ...guessedLetters.incorrect,
+    ].sort();
+
+    return combinedGuesses.map((letter) => {
+      return (
+        <p key={letter}>
+          {guessedLetters.incorrect.includes(letter) ? (
+            <span className="IncorrectGuess">{letter}</span>
+          ) : (
+            letter
+          )}
+        </p>
+      );
+    });
+  }
+
   function getRandomWord() {
+    setGameWon(undefined);
     setGameInProgress(true);
 
     const randomWord = dictionary[
@@ -49,27 +165,6 @@ export default function Hangman() {
     ].split('');
     setWord(randomWord);
 
-    setMysteryWord(new Array(randomWord.length).fill('_'));
-  }
-
-  function handleGuess(event) {
-    const guessedLetter = event.target.value;
-
-    console.log('word:', word);
-    console.log('mysteryWord:', mysteryWord);
-    console.log('guessedLetter', guessedLetter);
-
-    const matchIndices = word.reduce(
-      (acc, letter, index) =>
-        letter === guessedLetter ? [...acc, index] : acc,
-      []
-    );
-
-    const mysteryWordCopy = mysteryWord;
-    matchIndices.map((matchIndex) =>
-      mysteryWordCopy.splice(matchIndex, 1, guessedLetter)
-    );
-
-    setMysteryWord(mysteryWordCopy);
+    setWordBlanks(new Array(randomWord.length).fill('_'));
   }
 }
